196dc8d92f48f36975b5649a147bd662bbd6f13f
Alaoefyn <121045925+Alaoefyn@users.noreply.github.com>
2023-05-11 21:03:53 +0300

    Update Symbol_finder2.py

--- Symbol_finder2.py
+++ Symbol_finder2.py
@@ -1,1 +1,47 @@
 
+def get_math_symbol(symbol_name):
+    symbols = {
+        "integral": "∫",
+        "sum": "∑",
+        "product": "∏",
+        "infinity": "∞",
+        "partial": "∂",
+        "alpha": "α",
+        "beta": "β",
+        "gamma": "γ",
+        "delta": "δ",
+        "epsilon": "ε",
+        "theta": "θ",
+        "lambda": "λ",
+        "mu": "μ",
+        "sigma": "σ",
+        "omega": "ω",
+        "hash": "#",
+        "left_bracket": "[",
+        "right_bracket": "]",
+        "left_brace": "{",
+        "right_brace": "}",
+        "asterisk": "*",
+        "underscore": "_",
+        "pipe": "|",
+        "less_than": "<",
+        "greater_than": ">",
+        "apostrophe": "'",
+        "caret": "^",
+        "plus": "+",
+        "minus": "-",
+        "slash": "/",
+        "left_paren": "(",
+        "right_paren": ")",
+        "percent": "%",
+        "tilde": "∼"
+    }
+    if symbol_name in symbols:
+        return symbols[symbol_name]
+    else:
+        print("Symbol not found in archive")
+
+symbol_name = input("Enter the name of the math symbol you want to find: ")
+symbol = get_math_symbol(symbol_name)
+if symbol:
+    print(symbol)
